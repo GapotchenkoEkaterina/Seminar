c8b582909de636a2dfa28b1a4402e38532441e2e
Ekaterina <ekaterinagapotchenko@mail.ru>
2022-11-15 18:20:16 +0300

    Выполнено Домашнее задание к первому семинару

---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿// // Однострочный комментарий
+
+// /* Многострочный
+// ввп
+// вапв
+// вп
+// */
+
+// // напишите программу, которая на вход принимает число и выдает его квадрат
+
+// // Вариант 1
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         Console.WriteLine("Введи число: ");
+//         int number = int.Parse(Console.ReadLine()!);
+
+        // int sqr = number * number;
+
+        // Console.WriteLine($"Квадрат числа {number} = {sqr}");
+        /*
+        // Вариант 2
+
+        int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+        Console.WriteLine($"Квадрат числа {number} = {sqr1}");
+        */
+
+        // Вариант 3
+
+// Console.WriteLine("Введите числа ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите числа ");
+// int B = int.Parse(Console.ReadLine()!);
+// if (A==B/A)
+// {
+//     Console.Write("Да");
+// }else{Console.Write("Нет");
+
+// }
+
+// Вариант 4 
+
+// Console.WriteLine("Введите числа ");
+// int P = int.Parse(Console.ReadLine()!);
+// if (P==1) Console.WriteLine("Понедельник ");
+// if (P==2) Console.WriteLine("Вторник ");
+// if (P==3) Console.WriteLine("Среда ");
+// if (P==4) Console.WriteLine("Четверг ");
+// if (P==5) Console.WriteLine("Пятница ");
+// if (P==6) Console.WriteLine("Суббота ");
+// if (P==7) Console.WriteLine("Воскресенье ");
+
+// Домашнее задание: Задача №2
+
+// Console.WriteLine("Введите числа ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите числа ");
+// int B = int.Parse(Console.ReadLine()!);
+// if (A>B)
+// { 
+//     int max = A;
+//     Console.WriteLine($"Максимальное значение = {A}");
+    
+// }else{ int max = B;
+//     Console.WriteLine($"Максимальное значение = {B}");
+// }
+
+// Домашнее задание: Задача №4
+
+// Console.WriteLine("Введите число А ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число В ");
+// int B = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число С ");
+// int C = int.Parse(Console.ReadLine()!);
+// int max = A;
+// if (A>max) max=A;
+// if (B>max) max=B;
+// if (C>max) max=C;
+// Console.WriteLine($"Максимальное значение = {max}");
+
+//  Домашнее задание: Задача №6
+
+// Console.WriteLine("Введите число А ");
+// int A = int.Parse(Console.ReadLine()!);
+// if (A % 2 == 0)
+// {Console.WriteLine($"Число является четным = {A}");
+// }
+// else {Console.WriteLine($"Число является нечетным = {A}");
+// }
+
+//  Домашнее задание: Задача №8
+
+// Console.WriteLine("Введите число N ");
+// int N = int.Parse(Console.ReadLine()!);
+// int N2 = 1;
+// while (N2 <= N)
+// { 
+// if (N2 % 2 == 0)
+// Console.WriteLine($"Число является четным = {N2}");
+// N2++;
+// }
