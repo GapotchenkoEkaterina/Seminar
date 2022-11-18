f2762a9c645f021d3b8c8f52c1b8e115198166c5
Ekaterina <ekaterinagapotchenko@mail.ru>
2022-11-18 22:10:49 +0300

    Выполнено Домашнее задание ко второму семинару

--- Program.cs
+++ Program.cs
@@ -17,17 +17,17 @@
 //         Console.WriteLine("Введи число: ");
 //         int number = int.Parse(Console.ReadLine()!);
 
-        // int sqr = number * number;
+// int sqr = number * number;
 
-        // Console.WriteLine($"Квадрат числа {number} = {sqr}");
-        /*
-        // Вариант 2
+// Console.WriteLine($"Квадрат числа {number} = {sqr}");
+/*
+// Вариант 2
 
-        int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
-        Console.WriteLine($"Квадрат числа {number} = {sqr1}");
-        */
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} = {sqr1}");
+*/
 
-        // Вариант 3
+// Вариант 3
 
 // Console.WriteLine("Введите числа ");
 // int A = int.Parse(Console.ReadLine()!);
@@ -62,7 +62,7 @@
 // { 
 //     int max = A;
 //     Console.WriteLine($"Максимальное значение = {A}");
-    
+
 // }else{ int max = B;
 //     Console.WriteLine($"Максимальное значение = {B}");
 // }
@@ -102,3 +102,158 @@
 // Console.WriteLine($"Число является четным = {N2}");
 // N2++;
 // }
+
+// Второй семенар 
+
+// задача: Напишите программу, которая вводит случайное трехзначное число и удаляет вторую цифру
+
+// int number = new Random().Next(100, 1000);
+
+// Первый метод
+// Console.WriteLine(number);
+// Console.WriteLine($"Число без второй цифры иммеет вид: {number/100}{number%10}");
+// Console.WriteLine();
+// int result = (number/100)*10 + number%10;
+// Console.WriteLine(result);
+
+// Второй метод (строковый)
+
+// Console.WriteLine("Введите трехзначное число: ");
+// string Numbers = Console.ReadLine()!;
+
+// if(Numbers.Length !=3){
+//     Console.WriteLine ("Введено неверное число");
+// }
+// else {
+//     Console.WriteLine ($"Число без второй цифры иммеет вид: {Numbers[0]}{Numbers[2]}");
+// }
+
+// Первое задание
+
+// int number = new Random().Next(10, 100);
+// Console.WriteLine(number);
+
+//  if (number/10 > number%10)
+//  {Console.WriteLine (number/10);
+//  }
+//  else{
+//     Console.WriteLine (number%10);
+//  }
+
+// Второе задание
+//  Console.WriteLine("Введите число А ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число В ");
+// int B = int.Parse(Console.ReadLine()!);
+
+// if (A % B == 0)
+// {
+//     Console.WriteLine("Кратно");
+// }
+// else{
+//     Console.WriteLine($"Не кратно, остаток {A%B}");
+// }
+
+// Третье задание
+
+// Console.WriteLine("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+
+// if(num % 7 ==0 && num % 23 ==0 ) Console.WriteLine("Да");
+// else Console.WriteLine("Нет");
+
+// Четвертое задание
+//  Console.WriteLine("Введите число А ");
+// int A = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите число В ");
+// int B = int.Parse(Console.ReadLine()!);
+// if (A * A == B || B * B == A) Console.WriteLine("Является");
+// else Console.WriteLine("Не является");
+
+// Домашнее задание: 
+// Задача №10. Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа. Обязательна проверка на ввод чисел больше/меньше 3-х знаков
+
+// Console.WriteLine("Введите трехзначное число: ");
+// string Numbers = Console.ReadLine()!;
+
+// if(Numbers.Length !=3){
+//     Console.WriteLine ("Введено неверное число");
+// }
+// else {
+//     Console.WriteLine ($"Число без первой и третьей цифры иммеет вид: {Numbers[1]}");
+// }
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// Берем числа до 100000
+
+// Console.WriteLine("Введите число: ");
+// string Numbers = Console.ReadLine()!;
+// if(Numbers.Length <3)  Console.WriteLine("Третье число отсутствует");
+// else{
+// Console.WriteLine($"Третья цифра иммеет вид: {Numbers[2]}");
+// }
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным. Обязательна проверка на ввод числа <1 и >7
+
+
+// Console.WriteLine("Введите день недели: ");
+// int num = int.Parse(Console.ReadLine()!);
+
+// switch (num)
+// {
+//     case 1:{
+//         Console.WriteLine("Это будний день");
+//         break;
+//     }
+//         case 2:{
+//         Console.WriteLine("Это будний день");
+//         break;
+//     }
+//         case 3:{
+//         Console.WriteLine("Это будний день");
+//         break;
+//     }
+//         case 4:{
+//         Console.WriteLine("Это будний день");
+//         break;
+//     }
+//         case 5:{
+//         Console.WriteLine("Это будний день");
+//         break;
+//     }
+//         case 6:{
+//         Console.WriteLine("Это выходной день");
+//         break;
+//     }
+//         case 7:{
+//         Console.WriteLine("Это выходной день");
+//         break;
+//     }
+//     default:{
+//         Console.WriteLine("Такого дня не существует");
+//         break;
+//     }
+// }
+
+
+
+// Console.WriteLine("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+
+
+// if (num > 0 && num < 8) 
+
+//     if (num == 6 || num == 7)
+//     {
+//        Console.Write("Данный день является выходным");
+//     }      
+//     else
+//     {
+//     Console.Write("Данный день является будним");
+//        }
+// else 
+//      Console.Write("Ввели число не в пределах от 1 до 7, поэтому не возможно определить");
+    
